24df184bf6d8a9d808febc8f86b8a67010184e54
Meaww2 <childenchaos@gmail.com>
2025-04-19 00:20:05 +0700

    broken_function version1

--- broken_function/broken_function.rb
+++ broken_function/broken_function.rb
@@ -1,45 +1,44 @@
+# frozen_string_literal: true
 # my_functions.rb
 
 class BrokenFunctions
   def odd_even(n)
-    if number / 2 = 0 
-        puts "Even number."
-     else
-        puts "Odd number."
-     end
+    if n.even?
+      puts 'Even number.'
+    else
+      puts 'Odd number.'
+    end
   end
 
-  def def is_prime(n)
-    if n <= 1 
-       return false
-    else if n <= 3
-       return true
-    else if n % 2 == 0 && n % 3 == 0
-       return false
+  def is_prime(n)
+    if n <= 1
+      return false
+    elsif n <= 3
+      return true
+    elsif n.even? || (n % 3).zero?
+      return false
     end
- 
-    i = 5  
-    while i * i <= n
-    if n % i = 0 && n % (i + 2) = 0
-       return false
-    end
-       i += 6
-    end
-   
-    return true
- end
 
-  def find_longest_and_shortest_words()
-    longest_word == ""
-    shortest_word == words
+    i = 5
+    while i * i <= n
+      return false if (n % i).zero? && (n % (i + 2)).zero?
+
+      i += 2
+    end
+    true
+  end
+
+  def find_longest_and_shortest_words(words)
+    longest_word = ''
+    shortest_word = words[0]
     words.each do |word|
-      if word.length < longest_word.length
-          longest_word == word
-      else if word.length > shortest_word.length
-          shortest_word == word
+      if word.length > longest_word.length
+        longest_word = word
+      elsif word.length < shortest_word.length
+        shortest_word = word
       end
     end
 
-    return [shortest_word, longest_word]
+    [shortest_word, longest_word]
   end
 end
